ffdf4c324427fe599d3abba8ee32a8c30488d39c
ieadeleke <eadelekeife@yahoo.com>
2025-03-15 16:45:52 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export"
+  images: {
+    domains: ['backend.novatradar.com'], // Add your domain here
+  }
 };
 
 export default nextConfig;
